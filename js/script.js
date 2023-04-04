608dbe9c00d2f269998c6fc8da2a9cd3e6e387b7
Rodrigo Vergara <rodrigo.vegara.ctrabajo@gmail.com>
2023-04-03 21:55:38 -0400

    Primer push del codigo

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function () {
+    $("#botonRegistrar").click(function () {
+      alert("Se ha registrado el paciente");
+      $("#inputNombre").val("");
+      $("#inputApellido").val("");
+      $("#email").val("")
+      $("#phone").val("")
+      $("#adress").val("")
+      $("#pais").val("")
+      $("#ciudad").val("")
+    });
+  });
+var useradmin = "pepito";
+var passadmin = "pepe1234";
